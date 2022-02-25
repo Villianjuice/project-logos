26f9d18b88711ce32e8b1dcf86d2935384800242
Aslan <tavorktar@gmail.com>
2022-02-25 23:17:52 +0600

    rework 'delivery'

---
+++ js/countPerson.js
@@ -0,0 +1,21 @@
+const btnMinus = document.querySelector('[data-action="minus"]');
+const btnPlus = document.querySelector('[data-action="plus"]');
+const counter = document.querySelector('[data-counter]')
+
+// Отслеживаем клик на кнопку btnMinus
+btnMinus.addEventListener('click', function() {
+    // Проверяем чтобы счетчик был меньше 1
+    if(parseInt(counter.innerText) > 1) {
+        // Изменяем текст в счетчике, уменьшая его на 1
+        counter.innerText = --counter.innerText
+    }
+})
+
+// Отслеживаем клик на кнопку btnPlus
+btnPlus.addEventListener('click' ,function() {
+    // Изменяем текст в счетчике, увеличивая его на 1
+    if(parseInt(counter.innerText) < 15) {
+        // Изменяем текст в счетчике, уменьшая его на 1
+        counter.innerText = ++counter.innerText
+    }
+})
