95196bd66574aa62f7fbbd7ae67928f0b5f7843f
Aslan <tavorktar@gmail.com>
2022-03-03 01:25:15 +0600

    Added swiper and map

--- js/index.js
+++ js/index.js
@@ -1,17 +1,24 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-
-  
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
+const accordions = document.querySelectorAll(".accordion");
+for (const accordion of accordions) {
+  const panels = accordion.querySelectorAll(".accordion-panel");
+  for (const panel of panels) {
+    const head = panel.querySelector(".accordion-header");
+    head.addEventListener('click', () => {
+      for (const otherPanel of panels) {
+        if (otherPanel !== panel) {
+          otherPanel.classList.remove('accordion-expanded');
+        }
+      }
+      panel.classList.toggle('accordion-expanded');
+    });
+  }
 }
 
+
+ymaps.ready(init); 
+function init(){
+    var mPlayMap = new ymaps.Map("map",{center: [55.75985606898725,37.61054750000002],zoom: 12});
+    myMap.controls.add("zoomControl").add("typeSelector").add("mapTools");
+    var mycemark = new ymaps.Placemark([55.75985606898725,37.61054750000002]);
+    myMap.geoObjects.add(myPlacemark);	
+}
