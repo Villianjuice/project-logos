12aaf76e335fa579c820768deed4f4a2c3ab4576
Aslan <tavorktar@gmail.com>
2022-03-10 13:27:11 +0600

    fix bags

--- js/swiper.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    freeMode: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-});
